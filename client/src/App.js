6ffd020d5cab937d205089c4f91c0891fd3ec786
Andrew Murray <sckoobs@gmail.com>
2016-10-07 16:18:12 +0100

    Added client code using create-react-app and then ejected it

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Voting from './components/Voting';
+
+const pair = ['Trainspotting','28 Days Later'];
+
+class App extends Component {
+    render() {
+        return (
+            <Voting pair={pair} />
+        );
+    }
+}
+
+export default App;
+
+
+// <div className="App">
+//   <div className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <h2>Welcome to React</h2>
+//   </div>
+//   <p className="App-intro">
+//     To get started, edit <code>src/App.js</code> and save to reload, and then some.
+//   </p>
+// </div>

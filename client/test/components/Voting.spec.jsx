7be7f38d02977f587a3bab8645f59fcbbad19f54
Andrew Murray <sckoobs@gmail.com>
2016-10-14 17:00:54 +0100

    Added some more tests around the voting component although one is not passing.

--- client/test/components/Voting.spec.jsx
+++ client/test/components/Voting.spec.jsx
@@ -29,4 +29,58 @@ describe('Voting', () => {
 
         expect(votedWith).to.equal('Trainspotting');
     });
+
+    it('disabled buttons when user has voted', () => {
+        const component = renderIntoDocument(
+            <Voting pair={["Trainspotting", "28 Days Later"]}
+                    hasVoted="Trainspotting"/>
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons.length).to.equal(2);
+        expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+        expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+    });
+
+    it('adds voted label to the voted entry', () => {
+        const component = renderIntoDocument(
+            <Voting pair={["Trainspotting", "28 Days Later"]}
+                    hasVoted="Trainspotting"/>
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons[0].textContent).to.contain('Voted');
+    });
+
+    it('renders just the winner when the is one', () => {
+        const component = renderIntoDocument(
+            <Voting winner="Trainspotting" />
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+        
+        expect(buttons.length).to.equal(0);
+        const winner = ReactDOM.findDOMNode(component.refs.winner);
+        expect(winner).to.be.ok;
+        expect(winner.textContent).to.contain('Trainspotting');
+    });
+
+    it('renders as a pure component', () => {
+        const pair = ["Trainspotting", "28 Days Later"];
+        const container = document.createElement('div');
+
+        let component = ReactDOM.render(
+            <Voting pair={pair}/>, container
+        );
+
+        let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('Trainspotting');
+
+        pair[0] = 'Sunshine';
+        component = ReactDOM.render(
+            <Voting pair={pair}/>, container
+        );
+
+        firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('Trainspotting');
+    });
 });
